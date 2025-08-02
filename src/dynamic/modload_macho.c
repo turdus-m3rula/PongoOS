7fe70750a36769ebb0a8980cfc3f091b7cd448ad
meow <69592455+kok3shidoll@users.noreply.github.com>
2025-08-02 21:31:49 +0900

    just for palera1n
    Co-Authored-By: Nick Chan <towinchenmi@gmail.com>

--- src/dynamic/modload_macho.c
+++ src/dynamic/modload_macho.c
@@ -28,12 +28,189 @@
 #include <mach-o/loader.h>
 #include <mach-o/nlist.h>
 #include <mach-o/reloc.h>
+#include <lzma/lzmadec.h>
 
 // to replace with dlsym perhaps later on
 void* resolve_symbol(const char* name);
 
 void link_exports(struct pongo_exports* export);
 
+bool modload_buf(uint8_t* buf, uint32_t bufsz) {
+    iprintf("[modload_macho:i] Attempting to load a module\n");
+    if (bufsz >= 0x4000) {
+        //iprintf("enough bytes! %x\n", bufsz);
+        struct mach_header_64* mh = (void*) buf;
+        struct load_command* lc = (struct load_command*) (mh + 1);
+        if (mh->magic == MH_MAGIC_64) {
+            //puts("it's a mach-o!");
+            if (mh->filetype == MH_KEXT_BUNDLE) {
+                uint64_t filesz_expected = 0;
+                uint64_t vmsz_needed = 0;
+                uint64_t base_vmaddr = -1;
+                uint32_t segmentCount = 0;
+                const struct symtab_command *symtab = NULL;
+                const struct dysymtab_command *dysymtab;
+                for (int i=0; i<mh->ncmds; i++) {
+                    if (lc->cmd == LC_SEGMENT_64) {
+                        segmentCount++;
+                        struct segment_command_64 * sg = (struct segment_command_64*) lc;
+                        //iprintf("found %s, vmaddr %" PRIx64 ", vmsz %" PRIx64 ", fileoff %" PRIx64 ", filesize %" PRIx64 "\n", sg->segname, sg->vmaddr, sg->vmsize, sg->fileoff, sg->filesize);
+                        if (sg->vmaddr < base_vmaddr) base_vmaddr = sg->vmaddr;
+                        if (sg->fileoff + sg->filesize > filesz_expected)
+                            filesz_expected = sg->fileoff + sg->filesize;
+                        if (sg->vmaddr + sg->vmsize > vmsz_needed)
+                            vmsz_needed = sg->vmaddr + sg->vmsize;
+                    } else if (lc->cmd == LC_DYSYMTAB) {
+                        dysymtab = (struct dysymtab_command *)lc;
+                    } else if (lc->cmd == LC_SYMTAB) {
+                        symtab = (struct symtab_command *)lc;
+                    }
+                    lc = (struct load_command*)(((char*)lc) + lc->cmdsize);
+                }
+                vmsz_needed -= base_vmaddr;
+                //iprintf("need %" PRIx64 ", got %" PRIx64 "\n", filesz_expected, bufsz);
+                if (!(filesz_expected > bufsz)) {
+                    uint64_t entrypoint = 0;
+                    uint8_t * allocto = alloc_contig((vmsz_needed + 0x3FFF) & ~0x3FFF);
+                    uint64_t vma_base = linear_kvm_alloc(vmsz_needed);
+                    struct pongo_module_info* module = pongo_module_create(segmentCount);
+                    module->vm_base = vma_base;
+                    module->vm_end = vma_base + vmsz_needed;
+                    uint32_t segmentIndex = 0;
+                    
+                    //iprintf("need vm %" PRIx64 ", got %p, base %" PRIx64 "\n", vmsz_needed, allocto, base_vmaddr);
+                    struct load_command* lc = (struct load_command*) (mh + 1);
+                    for (int i=0; i<mh->ncmds; i++) {
+                        if (lc->cmd == LC_SEGMENT_64) {
+                            struct segment_command_64 * sg = (struct segment_command_64*) lc;
+                            struct pongo_module_segment_info* info = &module->segments[segmentIndex++];
+                            info->name = strdup(sg->segname);
+                            info->vm_addr = sg->vmaddr;
+                            info->vm_size = sg->vmsize;
+                            
+                            memset(allocto + sg->vmaddr - base_vmaddr, 0, sg->vmsize);
+                            memcpy(allocto + sg->vmaddr - base_vmaddr, buf + sg->fileoff, sg->filesize);
+                            
+                            vm_protect_t prots = 0;
+                            prots |= sg->initprot & VM_PROT_READ ? PROT_READ : 0;
+                            prots |= sg->initprot & VM_PROT_WRITE ? PROT_WRITE : 0;
+                            prots |= sg->initprot & VM_PROT_EXECUTE ? PROT_EXEC : 0;
+                            
+                            info->prot = prots;
+                            
+                            for (uint32_t page = 0; page < sg->vmsize; page += PAGE_SIZE) {
+                                //fiprintf(stderr, "mapping %" PRIx64 " (%" PRIx64 ", %" PRIx64 "), %x\n", vma_base + page + sg->vmaddr - base_vmaddr, sg->vmaddr, sg->vmsize, prots);
+                                uint64_t ppage = vatophys((uint64_t)(allocto + page + sg->vmaddr - base_vmaddr));
+                                vm_space_map_page_physical_prot(&kernel_vm_space, vma_base + page + sg->vmaddr - base_vmaddr, ppage, prots | PROT_KERN_ONLY);
+                            }
+                            
+                        }
+                        lc = (struct load_command*)(((char*)lc) + lc->cmdsize);
+                    }
+                    
+                    const struct relocation_info *extrel = (void *)((uintptr_t)mh + dysymtab->extreloff);
+                    const struct relocation_info *locrel = (void *)((uintptr_t)mh + dysymtab->locreloff);
+                    const struct nlist_64 *nlist = (struct nlist_64 *)((uintptr_t)mh + symtab->symoff);
+                    const char** modname = NULL;
+                    struct pongo_exports* exports = NULL;
+                    for (uint32_t sym_idx = 0; sym_idx < symtab->nsyms; sym_idx++) {
+                        const struct nlist_64 *nl = &nlist[sym_idx];
+                        uint32_t strx = nl->n_un.n_strx;
+                        const char *name = (const char *)((uintptr_t)mh + symtab->stroff + strx);
+                        // Check to see if this is the entry point symbol.
+                        int cmp = strcmp(name, "_module_entry");
+                        if (cmp == 0) {
+                            entrypoint = nl->n_value + (uint64_t)vma_base;
+                        }
+                        cmp = strcmp(name, "_module_name");
+                        if (cmp == 0) {
+                            modname = (const char**)(nl->n_value + (uint64_t)vma_base);
+                        }
+                        cmp = strcmp(name, "_exported_symbols");
+                        if (cmp == 0 && !exports) {
+                            exports = (struct pongo_exports*)(nl->n_value + (uint64_t)vma_base);
+                        }
+                    }
+                    for (uint32_t extrel_idx = 0; extrel_idx < dysymtab->nextrel; extrel_idx++) {
+                        const struct relocation_info *ri = &extrel[extrel_idx];
+                        // Skip non-extern or non-8-byte relocations.
+                        if (!ri->r_extern || ri->r_length != 3) {
+                            continue;
+                        }
+                        // Get the name of the symbol.
+                        const struct nlist_64 *nl = &nlist[ri->r_symbolnum];
+                        uint32_t strx = nl->n_un.n_strx;
+                        const char *name = (const char *)((uintptr_t)mh + symtab->stroff + strx);
+                        // Resolve the symbol to its runtime address.
+                        // XXX: line below was added during Linux ABI, is it ok to just drop this?
+                        //if (*name == '_') name++;
+                        void* symbol_value = resolve_symbol(name);
+                        if (symbol_value == 0) {
+                            puts("[modload_macho:!] load module: linking failed");
+                            return false;
+                        }
+                        // Find the offset of the relocation pointer in the virtually mapped Mach-O and
+                        // replace it with the resolved address of the symbol. r_address is the offset from
+                        // the first segment's vmaddr to the vmaddr of the pointer. Since we've put the
+                        // first segment's vmaddr at offset 0 in the mapping, this means that r_address is
+                        // exactly the offset into the mapping of the pointer we want to change.
+                        uint64_t vmoff = ri->r_address;
+                        *(uint64_t *)((uintptr_t)allocto + vmoff) = (uint64_t) symbol_value;
+                    }
+                    // Process the dysymtab's local relocations.
+                    for (uint32_t locrel_idx = 0; locrel_idx < dysymtab->nlocrel; locrel_idx++) {
+                        const struct relocation_info *ri = &locrel[locrel_idx];
+                        // Skip extern or non-8-byte relocations.
+                        if (ri->r_extern || ri->r_length != 3) {
+                            continue;
+                        }
+                        // Find the offset of the relocation pointer in the virtually mapped Mach-O and
+                        // slide it to the new base address.
+                        uint64_t vmoff = ri->r_address;
+                        uint64_t *reloc_ptr = (uint64_t *)((uintptr_t)allocto + vmoff);
+                        *reloc_ptr = *reloc_ptr - base_vmaddr + ((uint64_t)vma_base);
+                    }
+                    link_exports(exports);
+                    if (!entrypoint) panic("no entryp");
+                    
+                    iprintf("[modload_macho:+] Loaded module %s\n", modname ? *modname ? *modname : "<null>" : "<unknown>");
+                    flush_tlb();
+                    invalidate_icache();
+                    
+                    module->name = strdup(modname ? *modname ? *modname : "<null>" : "<unknown>");
+                    module->exports = exports;
+                    ((void (*)())entrypoint)();
+                    return true;
+                } else puts ("[modload_macho:!] load module: truncated load");
+            } else puts("[modload_macho:!] load module: need dylib");
+        } else puts("[modload_macho:!] load module: not mach-o");
+    } else puts("[modload_macho:!] load module: short read");
+    return false;
+}
+
+void modload_cmd(char* cmd, char* args) {
+    if (args[0] != '\0') {
+        size_t module_size;
+        if ((module_size = (int)strtoul(args, NULL, 0)) != 0) {
+            printf("args: %s, module_size: %zu\n", args, module_size);
+            void* module_buf = malloc(module_size);
+            if (!module_buf) panic("couldn't reserve heap for module");
+            int lzma_result = unlzma_decompress(module_buf, &module_size, loader_xfer_recv_data, loader_xfer_recv_count);
+            if (lzma_result == SZ_OK) {
+                modload_buf(module_buf, (uint32_t)module_size);
+                free(module_buf);
+            } else printf("module decompression failed\n");
+        } else {
+            printf("modload usage: modload [uncompressed size if compressed]\n");
+            return;
+        }
+    } else {
+        modload_buf(loader_xfer_recv_data, loader_xfer_recv_count);
+    }
+    loader_xfer_recv_size = 0;
+}
+
+/*
 void modload_cmd(const char *cmd, char *args) {
         iprintf("[modload_macho:i] Attempting to load a module\n");
         if (loader_xfer_recv_count >= 0x4000) {
@@ -185,3 +362,4 @@ void modload_cmd(const char *cmd, char *args) {
         } else puts("[modload_macho:!] load module: short read");
         loader_xfer_recv_count = 0;
 }
+*/
